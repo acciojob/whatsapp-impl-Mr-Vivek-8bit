860db2bc0adf6cbb8898d2168858fc21cdb4fceb
Mr-Vivek-8bit <viveksonu444@gmail.com>
2023-01-28 19:30:44 +0530

    solution-one

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -6,5 +6,38 @@ public class Message {
     private int id;
     private String content;
     private Date timestamp;
+    public Message(Date timestamp){
+        this.timestamp = timestamp;
+    }
+    public Message(int id, String content, Date timestamp) {
+        this.id = id;
+        this.content = content;
+        this.timestamp = timestamp;
+    }
+
+    public Message(int id, String content){
+        this.id = id;
+        this.content = content;
+        this.timestamp = new Date();
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+    public int getId(){
+        return id;
+    }
+    public void setContent(String content){
+        this.content = content;
+    }
+    public String getContent(){
+        return content;
+    }
+    public void setTimestamp(Date timestamp){
+        this.timestamp = timestamp;
+    }
+    public Date getTimestamp(){
+        return timestamp;
+    }
 
 }
